f1d61df241d971f4281c42d9d191c9b0114f393f
Abelaso12 <15221166@modelo.edu.mx>
2022-09-19 21:49:55 -0500

    Tarea 5

---
+++ 0x03_Operadores/3-area_circle.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+float area_circle(float radius)
+{
+float result;
+result=0;
+
+result= 3.14 *  (radius * radius);
+  
+  return (result);
+    
+}
---
+++ 0x03_Operadores/5-circumference.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+float Circumference(float radius)
+{
+float result;
+result=0;
+
+result= 2* (3.14* radius);
+
+return (result); 
+
+
+} 
